2f42d9278ea203097f9961bdd550b42a437b6842
Sopatun <zopatulin@gmail.com>
2026-05-28 20:38:40 -0600

    Cambios Esteticos

--- AppInventario/app/src/main/java/com/example/appinventario/ui/theme/Type.kt
+++ AppInventario/app/src/main/java/com/example/appinventario/ui/theme/Type.kt
@@ -1,7 +1,6 @@
 package com.example.appinventario.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -14,8 +13,7 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-        color = Color.Black
+        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
