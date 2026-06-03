4b96ad19229e01a017928b5158f4a0c484c2511f
JeRoch4021 <161788973+JeRoch4021@users.noreply.github.com>
2026-06-02 23:15:18 -0600

    Commenting errors

--- AppInventario/app/src/main/java/com/example/appinventario/data/network/InventarioApiService.kt
+++ AppInventario/app/src/main/java/com/example/appinventario/data/network/InventarioApiService.kt
@@ -4,8 +4,7 @@ import com.example.appinventario.data.remote.dto.*
 import retrofit2.http.*
 
 interface InventarioApiService {
-<<<<<<< HEAD
-    //LLAVEROS
+    /*//LLAVEROS
     @GET("llaveros")
     suspend fun getLlaveros(): List<LlaveroDto>
 
@@ -77,8 +76,8 @@ interface InventarioApiService {
         @Path("id") id: Int
     ): UsuarioDto
 
-    @POST("usuarios")
-=======
+    @POST("usuarios")*/
+
     @GET("llavero")
     suspend fun getLlaveros(): List<LlaveroDto>
 
@@ -101,7 +100,6 @@ interface InventarioApiService {
     suspend fun getUsuarios(): List<UsuarioDto>
 
     @POST("usuario")
->>>>>>> 82627cc9f1e63429e2150a0805d0884f000a183c
     suspend fun createUsuario(@Body usuario: UsuarioDto): UsuarioDto
 
     @PUT("usuarios/{id}")
